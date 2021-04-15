5193214365a5b05ef735ef2dfb10338489782836
kking117 <41817289+kking117@users.noreply.github.com>
2021-04-15 19:40:48 +1000

    2.0.0 update

--- AurelioniteStuck.cs
+++ AurelioniteStuck.cs
@@ -1,7 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
 using BepInEx;
 using BepInEx.Configuration;
-using R2API.Utils;
 using RoR2;
 using UnityEngine;
 using UnityEngine.Networking;
@@ -9,21 +9,31 @@ using UnityEngine.SceneManagement;
 
 namespace AurelioniteStuck
 {
-	[BepInPlugin("com.kking117.AurelioniteStuck", "AurelioniteStuck", "1.0.0")]
-	[BepInDependency("com.bepis.r2api", "2.5.14")]
-	public class mainstuff : BaseUnityPlugin
+	[BepInPlugin("com.kking117.AurelioniteStuck", "AurelioniteStuck", "2.0.0")]
+	[BepInDependency("com.bepis.r2api")]
+	public class MainPlugin : BaseUnityPlugin
 	{
-		private CharacterMaster[] TitanList = new CharacterMaster[10];
+		private static GameObject TitanHostile = Resources.Load<GameObject>("prefabs/charactermasters/TitanGoldMaster");
+		private static GameObject TitanFriendly = Resources.Load<GameObject>("prefabs/charactermasters/TitanGoldAllyMaster");
 
-		private float[] NextTitanSpawn = new float[3]
+		private CombatSquad[] TitanTeam = new CombatSquad[4];
+
+		public static ConfigEntry<int> Config_SpawnOnPhase;
+		public static ConfigEntry<bool> Config_KillOnSteal;
+		public static ConfigEntry<bool> Config_SpawnOnSteal;
+		public static ConfigEntry<bool> Config_Debug;
+
+		private float[] NextTitanSpawn = new float[4]
 		{
+			-1f,
 			-1f,
 			-1f,
 			-1f
 		};
 
-		private float[] NextTitanKill = new float[3]
+		private float[] NextTitanKill = new float[4]
 		{
+			-1f,
 			-1f,
 			-1f,
 			-1f
@@ -31,14 +41,15 @@ namespace AurelioniteStuck
 
 		private int BossSeedCount;
 
-		Vector3 spawnposition = new Vector3(-88.2f, 491.95f, -2.54f);
+		Vector3 spawnposition = new Vector3(-87.4f, 491.95f, -2.54f);
 		Quaternion spawnrotation = new Quaternion(0.0f, 0.5f, 0.0f, 0.0f);
 		public void Awake()
 		{
+			ReadConfig();
 			On.RoR2.Run.Start += (orig, self) =>
 			{
 				orig(self);
-				for (int i = 0; i < 3; i++)
+				for (int i = 0; i < NextTitanSpawn.Length; i++)
 				{
 					NextTitanSpawn[i] = -1f;
 					NextTitanKill[i] = -1f;
@@ -47,9 +58,10 @@ namespace AurelioniteStuck
 			On.RoR2.Run.FixedUpdate += (orig, self) =>
 			{
 				orig(self);
-				if (SceneManager.GetActiveScene().name.Equals("moon"))
+				string scenename = SceneManager.GetActiveScene().name;
+				if (scenename == "moon" || scenename == "moon2")
 				{
-					for (int i = 0; i < 3; i++)
+					for (int i = 0; i < NextTitanSpawn.Length; i++)
 					{
 						if (NextTitanSpawn[i] >= 0f)
 						{
@@ -57,11 +69,11 @@ namespace AurelioniteStuck
 							{
 								if (i == 2)
 								{
-									SpawnAurelionite((TeamIndex)i, BossSeedCount);
+									CreateTitanGoldCombatSquad((TeamIndex)i, BossSeedCount);
 								}
 								else
 								{
-									SpawnAurelionite((TeamIndex)i, GetTeamSeedCount((TeamIndex)i));
+									CreateTitanGoldCombatSquad((TeamIndex)i, GetTeamSeedCount((TeamIndex)i));
 								}
 								NextTitanSpawn[i] = -1f;
 							}
@@ -70,7 +82,7 @@ namespace AurelioniteStuck
 						{
 							if (NextTitanKill[i] <= Time.time)
 							{
-								KillTitans((TeamIndex)i);
+								KillTitanSquad(TitanTeam[i]);
 								NextTitanKill[i] = -1f;
 							}
 						}
@@ -81,7 +93,7 @@ namespace AurelioniteStuck
 			On.EntityStates.Missions.BrotherEncounter.Phase1.OnEnter += (orig, self) =>
 			{
 				orig(self);
-				if (spawnonphase == 1)
+				if (Config_SpawnOnPhase.Value == 1)
 				{
 					NextTitanSpawn[1] = Time.time + 5.0f;
 				}
@@ -89,7 +101,7 @@ namespace AurelioniteStuck
 			On.EntityStates.Missions.BrotherEncounter.Phase2.OnEnter += (orig, self) =>
 			{
 				orig(self);
-				if (spawnonphase == 2)
+				if (Config_SpawnOnPhase.Value == 2)
 				{
 					NextTitanSpawn[1] = Time.time + 3.0f;
 				}
@@ -97,7 +109,7 @@ namespace AurelioniteStuck
 			On.EntityStates.Missions.BrotherEncounter.Phase3.OnEnter += (orig, self) =>
 			{
 				orig(self);
-				if (spawnonphase == 3)
+				if (Config_SpawnOnPhase.Value == 3)
 				{
 					NextTitanSpawn[1] = Time.time + 5.0f;
 				}
@@ -105,9 +117,12 @@ namespace AurelioniteStuck
 			On.EntityStates.Missions.BrotherEncounter.Phase4.OnEnter += (orig, self) =>
 			{
 				orig(self);
-				if (spawnonsteal)
-				{
+				if(Config_KillOnSteal.Value)
+                {
 					NextTitanKill[1] = Time.time + 7.5f;
+				}
+				if (Config_SpawnOnSteal.Value)
+				{
 					BossSeedCount = GetTeamSeedCount(TeamIndex.Player);
 					NextTitanSpawn[2] = Time.time + 12.0f;
 				}
@@ -115,117 +130,172 @@ namespace AurelioniteStuck
 			On.EntityStates.Missions.BrotherEncounter.EncounterFinished.OnEnter += (orig, self) =>
 			{
 				orig(self);
-				if (spawnonsteal)
+				for(int i = 0; i<NextTitanSpawn.Length; i++)
+                {
+					NextTitanSpawn[i] = -1f;
+					NextTitanKill[i] = Time.time + 2.5f;
+				}
+				
+			};
+			On.RoR2.GoldTitanManager.OnBossGroupStartServer += (orig, bossGroup) =>
+			{
+				bool IsMithrix = false;
+				CombatSquad combatSquad = bossGroup.combatSquad;
+				using (IEnumerator<CharacterMaster> enumerator = combatSquad.readOnlyMembersList.GetEnumerator())
 				{
-					NextTitanSpawn[2] = -1f;
-					NextTitanKill[2] = Time.time + 2.5f;
+					while (enumerator.MoveNext())
+					{
+						if (enumerator.Current.masterIndex == MasterCatalog.FindMasterIndex("BrotherHurtMaster"))
+						{
+							IsMithrix = true;
+							break;
+						}
+					}
+				}
+				if(IsMithrix)
+                {
+                }
+				else
+                {
+					orig(bossGroup);
 				}
 			};
-
 			int GetTeamSeedCount(TeamIndex team)
 			{
 				int SeedCount = 0;
-				for (int i = 0; i < RoR2.PlayerCharacterMasterController.instances.Count; i++)
+				SeedCount = Util.GetItemCountForTeam(team, RoR2Content.Items.TitanGoldDuringTP.itemIndex, true, true);
+				return SeedCount;
+			};
+			void CreateTitanGoldCombatSquad(TeamIndex team, int SeedCount)
+			{
+				if (SeedCount > 0)
 				{
-					CharacterBody charbody = RoR2.PlayerCharacterMasterController.instances[i].master.GetBody();
-					if (charbody)
-					{
-						TeamComponent teamComponent = charbody.teamComponent;
-						if (teamComponent.teamIndex == team)
+					CombatSquad combatSquad;
+					if(team == TeamIndex.Player)
+                    {
+						combatSquad = UnityEngine.Object.Instantiate<GameObject>(Resources.Load<GameObject>("Prefabs/NetworkedObjects/Encounters/MonstersOnShrineUseEncounter")).GetComponent<CombatSquad>();
+					}
+					else
+                    {
+						combatSquad = UnityEngine.Object.Instantiate<GameObject>(Resources.Load<GameObject>("Prefabs/NetworkedObjects/Encounters/ShadowCloneEncounter")).GetComponent<CombatSquad>();
+					}
+					int SpawnCount = 1;
+					//This is just here really.
+					//Like, swarms doesn't double aurelionite so this shouldn't be needed
+					/*if (RunArtifactManager.instance.IsArtifactEnabled(ArtifactCatalog.FindArtifactDef("Swarms")))
+                    {
+						if (Config_Debug.Value)
 						{
-							HealthComponent healthComponent = charbody.healthComponent;
-							if (healthComponent && healthComponent.health > 0)
+							print("Doubled Aurelionite for Swarms");
+						}
+					}*/
+					for (int i = 0; i < SpawnCount; i++)
+					{
+						Vector3 offsetpos = GetRandomSpawnPos();
+						CharacterMaster component = new MasterSummon
+						{
+							masterPrefab = team == TeamIndex.Player ? TitanFriendly : TitanHostile,
+							position = spawnposition + offsetpos,
+							summonerBodyObject = null,
+							ignoreTeamMemberLimit = true,
+							teamIndexOverride = team,
+						}.Perform();
+						AddTitanInventory(component, SeedCount);
+						combatSquad.AddMember(component);
+					}
+					if (combatSquad)
+					{
+						NetworkServer.Spawn(combatSquad.gameObject);
+						RegisterTitanSquad(combatSquad, team);
+					}
+				}
+			}
+
+			Vector3 GetRandomSpawnPos()
+			{
+				Vector3 angle = new Vector3(1f, 1f, 1f);
+				//Rotate the angle by a random degree
+				float rotation = UnityEngine.Random.Range(0.0f, 360.0f);
+				float x = angle[0];
+				float y = angle[2];
+				angle[1] = 0f;
+				angle[0] = x * (float)Math.Cos(rotation) - y * (float)Math.Sin(rotation);
+				angle[2] = x * (float)Math.Sin(rotation) + y * (float)Math.Cos(rotation);
+				//Then normalize the angles
+				angle = Vector3.Normalize(angle);
+				angle *= UnityEngine.Random.Range(20f, 40f);
+				print(angle);
+				return angle;
+			}
+			void AddTitanInventory(CharacterMaster titan, int seeds)
+            {
+				float num = 1f;
+				float num2 = 1f;
+				num += Run.instance.difficultyCoefficient / 2f;
+				num2 += Run.instance.difficultyCoefficient / 2f;
+				num *= Mathf.Pow((float)seeds, 1f);
+				num2 *= Mathf.Pow((float)seeds, 0.5f);
+				if (titan.teamIndex != TeamIndex.Player)
+				{
+					num = Mathf.Pow(num2, 0.5f);
+					num2 = Mathf.Pow(num2, 0.5f);
+				}
+				titan.inventory.GiveItem(RoR2Content.Items.BoostHp.itemIndex, Mathf.RoundToInt((num - 1f) * 10f));
+				titan.inventory.GiveItem(RoR2Content.Items.BoostDamage.itemIndex, Mathf.RoundToInt((num2 - 1f) * 10f));
+				if (Config_Debug.Value)
+				{
+					print("Aurelionite spawned with " + (10 + Mathf.RoundToInt((num - 1f) * 10f)) * 10.0f + "% health.");
+					print("Aurelionite spawned with " + (10 + Mathf.RoundToInt((num2 - 1f) * 10f)) * 10.0f + "% damage.");
+				}
+			}
+			void RegisterTitanSquad(CombatSquad combatSquad, TeamIndex team)
+			{
+				int slot = (int)team;
+				if(slot>= 0 && slot<=TitanTeam.GetLength(0))
+                {
+					KillTitanSquad(TitanTeam[slot]);
+					TitanTeam[slot] = combatSquad;
+				}
+			}
+			void KillTitanSquad(CombatSquad combatSquad)
+            {
+				CharacterMaster[] HitList = new CharacterMaster[0];
+				if (combatSquad)
+				{
+					using (IEnumerator<CharacterMaster> enumerator = combatSquad.readOnlyMembersList.GetEnumerator())
+					{
+						while (enumerator.MoveNext())
+						{
+							if (enumerator.Current)
 							{
-								CharacterMaster master = charbody.master;
-								if (master)
+								if (enumerator.Current.GetBody())
 								{
-									SeedCount += master.inventory.GetItemCount(ItemIndex.TitanGoldDuringTP);
+									if (enumerator.Current.GetBody().healthComponent.alive)
+									{
+										Array.Resize<CharacterMaster>(ref HitList, HitList.Length + 1);
+										HitList[HitList.Length - 1] = enumerator.Current;
+									}
 								}
 							}
 						}
 					}
 				}
-				return SeedCount;
-			};
-			void SpawnAurelionite(TeamIndex team, int SeedCount)
-			{
-				if (SeedCount > 0)
-				{
-					float deadrangeA = 5f;
-					float maxrangeA = 15f;
-					if (UnityEngine.Random.Range(0f, 1f)>0.5f)
+				for(int i = 0; i< HitList.Length; i++)
+                {
+					if(HitList[i])
                     {
-						deadrangeA *= 1f;
-						maxrangeA *= 1f;
-					}
-					float deadrangeB = 5f;
-					float maxrangeB = 15f;
-					if (UnityEngine.Random.Range(0f, 1f) > 0.5f)
-					{
-						deadrangeB *= 1f;
-						maxrangeB *= 1f;
-					}
-					CharacterMaster titan = new MasterSummon
-					{
-						masterPrefab = MasterCatalog.GetMasterPrefab(MasterCatalog.FindMasterIndex("TitanGoldMaster")),
-						position = spawnposition - new Vector3(UnityEngine.Random.Range(deadrangeA, maxrangeA), 0f, UnityEngine.Random.Range(deadrangeB, maxrangeB)),
-						rotation = spawnrotation,
-						summonerBodyObject = null,
-						ignoreTeamMemberLimit = true,
-						teamIndexOverride = team,
-					}.Perform();
-
-					if (titan)
-					{
-						float num = 1f;
-						float num2 = 1f;
-						num2 += Run.instance.difficultyCoefficient / 8f;
-						num += Run.instance.difficultyCoefficient / 2f;
-						num *= Mathf.Pow((float)SeedCount, 1f);
-						num2 *= Mathf.Pow((float)SeedCount, 0.5f);
-						if (team == TeamIndex.Monster)
-						{
-							num2 *= 0.75f; //nerf its damage into the ground for the player's sake
-						}
-						titan.inventory.GiveItem(ItemIndex.BoostHp, Mathf.RoundToInt((num - 1f) * 10f));
-						titan.inventory.GiveItem(ItemIndex.BoostDamage, Mathf.RoundToInt((num2 - 1f) * 10f));
-						RegisterTitan(titan);
-					}
-				}
-			};
-			void RegisterTitan(CharacterMaster titan)
-			{
-				for (int i = 0; i < TitanList.GetLength(0); i++)
-				{
-					if (TitanList[i] == null)
-					{
-						TitanList[i] = titan;
-						MonoBehaviour.print("aure id = " + i);
-						break;
-					}
-				}
-			}
-			void KillTitans(TeamIndex team)
-			{
-				for (int i = 0; i < TitanList.GetLength(0); i++)
-				{
-					if (TitanList[i])
-					{
-						if (TitanList[i].teamIndex == team)
-						{
-							TitanList[i].TrueKill();
-							TitanList[i] = null;
-						}
-					}
-				}
+						HitList[i].TrueKill();
+                    }
+                }
 			}
 		}
-		public static ConfigFile CustomConfigFile = new ConfigFile(Paths.ConfigPath + "\\kking117.AurelioniteStuck.cfg", true);
 
-		public static ConfigEntry<int> spawnonphase_config = CustomConfigFile.Bind<int>("AurelioniteStuck Config", "Spawn On Boss Phase", 2, "Makes Aurelionite spawn on this phase of the Final Boss if players have the Halcyon Seed. (0 - 3) (0 = don't spawn)");
-		private int spawnonphase = spawnonphase_config.Value;
-
-		public static ConfigEntry<bool> spawnonsteal_config = CustomConfigFile.Bind<bool>("AurelioniteStuck Config", "Spawn Phase Four", true, "Allows the Final Boss to spawn his own Aurelionite when he steals all items. (Will get automatically killed when the boss is defeated.)");
-		private bool spawnonsteal = spawnonsteal_config.Value;
+		public void ReadConfig()
+        {
+			Config_SpawnOnPhase = Config.Bind<int>(new ConfigDefinition("AurelioniteStuck", "Spawn Phase"), 2, new ConfigDescription("Spawns Aurelionite on this phase during the Final Boss if players have the Halcyon Seed. (0-3) (0 = Don't spawn)", null, Array.Empty<object>()));
+			Config_SpawnOnSteal = Config.Bind<bool>(new ConfigDefinition("AurelioniteStuck", "Spawn On Last Phase"), true, new ConfigDescription("Spawns an Aurelionite for Mithrix on the item steal phase. (Gets automatically killed if Mithrix dies)", null, Array.Empty<object>()));
+			Config_KillOnSteal = Config.Bind<bool>(new ConfigDefinition("AurelioniteStuck", "Kill On Last Phase"), true, new ConfigDescription("Kill the player's Aurelionite on Mithrix's item steal phase.", null, Array.Empty<object>()));
+			Config_Debug = Config.Bind<bool>(new ConfigDefinition("AurelioniteStuck", "Spam Console"), false, new ConfigDescription("Prints console messages for debug purposes.", null, Array.Empty<object>()));
+		}
 	}
 }
