e7a6c7871b77cb84153d8ebaea1f1b08b1ed0e59
kking117 <41817289+kking117@users.noreply.github.com>
2024-09-02 15:15:19 +1000

    Update 1.0.1 (Real)

--- Smaller Mods/MochaAntler.cs
+++ Smaller Mods/MochaAntler.cs
@@ -8,6 +8,7 @@ using R2API;
 using R2API.Utils;
 using Mono.Cecil.Cil;
 using MonoMod.Cil;
+using UnityEngine.AddressableAssets;
 
 namespace MochaAntler
 {
@@ -17,7 +18,7 @@ namespace MochaAntler
 	{
 		public const string MODUID = "com.kking117.MochaAntler";
 		public const string MODNAME = "MochaAntler";
-		public const string MODVERSION = "1.0.0";
+		public const string MODVERSION = "1.0.1";
 
 		public static float Stack_MoveSpeed;
 		public static float Stack_Armor;
@@ -30,9 +31,20 @@ namespace MochaAntler
 		{
 			RecalculateStatsAPI.GetStatCoefficients += GetStatCoefficients;
 			UpdateText();
+			UpdateItemDef();
 			IL.RoR2.HealthComponent.TakeDamageProcess += new ILContext.Manipulator(IL_DisableOldEffect);
 		}
-
+		private void UpdateItemDef()
+		{
+			ItemDef itemDef = Addressables.LoadAssetAsync<ItemDef>("RoR2/DLC2/Items/NegateAttack/NegateAttack.asset").WaitForCompletion();
+			if (itemDef)
+			{
+				List<ItemTag> itemTags = itemDef.tags.ToList();
+				itemTags.Remove(ItemTag.AIBlacklist);
+				itemTags.Remove(ItemTag.BrotherBlacklist);
+				itemDef.tags = itemTags.ToArray();
+			}
+		}
 		private void UpdateText()
         {
 			string pickup = "Slightly increase";
